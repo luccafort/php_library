bdceea47426e7158c31225b5be3301758e5a2bb6
luccafort <luccafort@gmail.com>
2013-02-16 11:37:55 +0900

    レスポンスクラスの処理を簡易化

--- php_library/util/Response.php
+++ php_library/util/Response.php
@@ -1,17 +1,16 @@
 <?php
+/**
+ * レスポンス管理
+ * @author luccafort
+ *
+ */
 class Util_Response
 {
-	const HTTP_VERSION = 'HTTP/1.1';
-
 	/**
-	 * 処理中.
-	 *
-	 * リクエストされた処理の実行時間が長時間かかると想定される場合に使用
-	 * クライアントがタイムアウトエラーになってしまわないようにするためのコード
-	 *
-	 * @var int
+	 * HTTPヘッダ
+	 * @var String
 	 */
-	const PROCESSING = 102;
+	const HTTP_VERSION = 'HTTP/1.1';
 
 	/**
 	 * リクエスト成功.
@@ -21,43 +20,6 @@ class Util_Response
 	 * @var int
 	 */
 	const SUCCESS = 200;
-
-	/**
-	 * 生成成功.
-	 *
-	 * リソースが生成された場合などに使用
-	 *
-	 * @var int
-	 */
-	const CREATED = 201;
-
-	/**
-	 * リクエスト受容.
-	 *
-	 * リクエストの受容までを保証する際に使用
-	 * 会員登録時などにタイムアウトエラーによる多重登録を防ぐ際などを想定
-	 *
-	 * @var int
-	 */
-	const ACCEPTED = 202;
-
-	/**
-	 * リクエストされたデータが存在しない
-	 *
-	 * 指定されたリソースデータがない場合などに使用
-	 *
-	 * @var int
-	 */
-	const NOT_CONTENT_DATA = 204;
-
-	/**
-	 * リセットリクエスト
-	 *
-	 * リクエスト成功後、クライアント側でデータのリセットが必要な場合に使用
-	 *
-	 * @var int
-	 */
-	const RESET_CONTENT_DATA = 205;
 
 	/**
 	 * 不正リクエスト
@@ -98,47 +60,28 @@ class Util_Response
 	const NOT_FOUND = 404;
 
 	/**
-	 * タイムアウトエラー
-	 *
-	 * リクエストが時間内に完了していない場合に使用.
-	 *
-	 * @var int
-	 */
-	const REQUEST_TIMEOUT = 408;
-
-	/**
-	 * リクエストURIが長すぎる
-	 *
-	 * リクエストURIが長すぎる場合に使用.
-	 *
-	 * @var int
-	 */
-	const REQUEST_URI_TOO_LARGE = 414;
-
-	/**
 	 * 不明なエラー
 	 * @var int
 	 */
 	const UNKNOWN_ERROR = 500;
 
-	public static function sendStatusCode( $status, $message = '' )
+	/**
+	 * レスポンスヘッダーを返す.
+	 * @param int $status
+	 * @param String $message
+	 * @throws Exception
+	 */
+	public static function setResponse( $status, $message = '' )
 	{
 		if( false == Util_Validator::isNumeric( $status ) ){
 			throw new Exception( 'status code is not numeric.' );
 		}
 		switch( $status ){
-			case self::PROCESSING:
 			case self::SUCCESS:
-			case self::CREATED:
-			case self::ACCEPTED:
-			case self::NOT_CONTENT_DATA:
-			case self::RESET_CONTENT_DATA:
 			case self::BAD_REQUEST:
 			case self::UNAUTHORIZED:
 			case self::FOBIDDEN:
 			case self::NOT_FOUND:
-			case self::REQUEST_TIMEOUT:
-			case self::REQUEST_URI_TOO_LARGE:
 			case self::UNKNOWN_ERROR:
 				$value = strval( self::HTTP_VERSION . ' ' . $status . ' ' . $message );
 				header( $value );
